a6da4d4f7adfae68383a36c9074cb4cfca61175c
Emiliyan Gospodinov <emonidi@gmail.com>
2022-01-26 18:17:09 +0100

    added service worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -10,6 +10,8 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://cra.link/PWA
 
+// eslint-disable-next-line no-restricted-globals
+const ignored:any = (self as any).__WB_MANIFEST;
 const isLocalhost = Boolean(
     window.location.hostname === 'localhost' ||
       // [::1] is the IPv6 localhost address.
